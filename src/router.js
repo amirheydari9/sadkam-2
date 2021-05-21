570fa25cc2081ed9fd7f8860481ee150c09929c0
amir <amirheydari9@gmail.com>
2021-05-21 08:18:07 -0700

    main.js

--- src/router.js
+++ src/router.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import { authService } from './service/authService'
+import { permission } from './plugins/permission'
+import store from './store/store'
 
 Vue.use(Router)
 
-export default new Router({
+const routes = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       component: () => import('@/views/dashboard/Index'),
+      meta: {
+        auth: true,
+      },
       children: [
         // Dashboard
         {
@@ -17,46 +23,115 @@ export default new Router({
           path: '',
           component: () => import('@/views/dashboard/Dashboard'),
         },
-        // Pages
+        // pages
         {
-          name: 'User Profile',
-          path: 'pages/user',
-          component: () => import('@/views/dashboard/pages/UserProfile'),
+          name: 'Users',
+          path: '/users',
+          component: () => import('@/views/pages/User/Index.vue'),
+          meta: {
+            permission: 'user_manager',
+          },
         },
         {
-          name: 'Notifications',
-          path: 'components/notifications',
-          component: () => import('@/views/dashboard/component/Notifications'),
+          name: 'Organizations',
+          path: '/organizations',
+          component: () => import('@/views/pages/Organization/Index.vue'),
+          meta: {
+            isSuperAdmin: true,
+          },
         },
         {
-          name: 'Icons',
-          path: 'components/icons',
-          component: () => import('@/views/dashboard/component/Icons'),
+          name: 'Products',
+          path: '/products',
+          component: () => import('@/views/pages/Product/Index.vue'),
+          meta: {
+            permission: 'orders',
+          },
         },
         {
-          name: 'Typography',
-          path: 'components/typography',
-          component: () => import('@/views/dashboard/component/Typography'),
+          name: 'Episodes',
+          path: '/episodes',
+          component: () => import('@/views/pages/Episode/Index.vue'),
+          meta: {
+            permission: 'orders',
+          },
         },
-        // Tables
         {
-          name: 'Regular Tables',
-          path: 'tables/regular-tables',
-          component: () => import('@/views/dashboard/tables/RegularTables'),
+          name: 'AssessmentRequest',
+          path: '/assessmentRequest',
+          component: () => import('@/views/pages/AssessmentRequest/Index.vue'),
+          meta: {
+            hasAssessmentRequestPermission: true,
+          },
         },
-        // Maps
         {
-          name: 'Google Maps',
-          path: 'maps/google-maps',
-          component: () => import('@/views/dashboard/maps/GoogleMaps'),
+          name: 'AssessmentRequest1',
+          path: '/assessmentRequest1',
+          component: () => import('@/views/pages/AssessmentRequest1/Index.vue'),
+          meta: {
+            hasAssessmentRequestPermission: true,
+          },
         },
-        // Upgrade
         {
-          name: 'Upgrade',
-          path: 'upgrade',
-          component: () => import('@/views/dashboard/Upgrade'),
+          name: 'ViewAssessmentRequest',
+          path: '/viewAssessmentRequest',
+          component: () => import('@/views/pages/ViewAssessmentRequest/Index.vue'),
+          meta: {
+            permission: 'orders',
+          },
         },
+
       ],
     },
   ],
 })
+
+routes.beforeEach(async (to, from, next) => {
+  if (authService().existToken() && !store.getters.getCurrentUser) {
+    await store.dispatch('handleCurrentUser', authService().decodeToken(authService().getToken()))
+  }
+
+  if ((to.meta.guest || to.matched.some(parent => parent.meta.guest)) &&
+    authService().existToken()) {
+    return next({ name: 'Panel' })
+  }
+
+  if ((to.meta.auth || to.matched.some(parent => parent.meta.auth)) &&
+    !authService().existToken()) {
+    return next({ name: 'Login' })
+  }
+
+  if (store.getters.getCurrentUser && to.meta.permission) {
+    if (permission().can(to.meta.permission)) {
+      return next()
+    } else {
+      await store.dispatch('logout')
+      return next({ name: 'Login' })
+    }
+  }
+
+  if (store.getters.getCurrentUser && to.meta.isSuperAdmin) {
+    if (permission().isSuperAdmin()) {
+      return next()
+    } else {
+      await store.dispatch('logout')
+      return next({ name: 'Login' })
+    }
+  }
+  if (store.getters.getCurrentUser && to.meta.hasAssessmentRequestPermission) {
+    if (permission().hasAssessmentRequestPermission()) {
+      return next()
+    } else {
+      await store.dispatch('logout')
+      return next({ name: 'Login' })
+    }
+  }
+  // if ((to.meta.phoneVerified || to.matched.some(parent => parent.meta.phoneVerified))
+  //     && !store.state.loginPhone) {
+  //     console.log(store.state.loginPhone)
+  //     return next({name: 'Login'});
+  // }
+  return next()
+})
+
+export default routes
