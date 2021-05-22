3040fecc211ac8cf59db0982adda976e3a12b15d
amir <amirheydari9@gmail.com>
2021-05-22 14:00:01 -0700

    add toast

--- src/views/pages/StaticData/Index.vue
+++ src/views/pages/StaticData/Index.vue
@@ -1,10 +1,41 @@
 <template>
-<p>dd</p>
+  <v-container>
+    <v-tabs
+      v-model="tabsMenu"
+      class="mt-5"
+      color="grey darken-3"
+    >
+      <v-tab
+        href="#rules"
+      >
+        مدیریت رول ها
+      </v-tab>
+      <v-tab
+        href="#generes"
+      >
+        مدیریت ژانرها
+      </v-tab>
+      <v-tab
+        href="#productCategory"
+      >
+        مدیریت دسته بندی محصولات
+      </v-tab>
+    </v-tabs>
+    <v-divider />
+  </v-container>
 </template>
 
 <script>
   export default {
     name: 'Index',
+    data () {
+      return {
+        tabsMenu: null,
+      }
+    },
+    mounted () {
+      this.$store.dispatch('staticData/fetchRulesList')
+    },
   }
 </script>
 
