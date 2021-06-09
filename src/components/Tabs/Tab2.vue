2174019030ea4938f6a445da02619dfaf23b0f44
amir <amirheydari9@gmail.com>
2021-06-08 22:42:32 -0700

    pagination for tab 2 , tab3

--- src/components/Tabs/Tab2.vue
+++ src/components/Tabs/Tab2.vue
@@ -46,6 +46,13 @@
       no-results-text="اطلاعاتی یافت نشد"
       class="elevation-1 w-100 mt-3"
       :items-per-page="5"
+      :footer-props="{
+      showFirstLastPage: true,
+      firstIcon: 'mdi-arrow-collapse-left',
+      lastIcon: 'mdi-arrow-collapse-right',
+      prevIcon: 'mdi-plus',
+      nextIcon: 'mdi-minus'
+    }"
     >
       <template v-slot:top>
         <v-toolbar
