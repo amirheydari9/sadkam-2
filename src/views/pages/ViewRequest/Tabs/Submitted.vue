abfb520325014b8b373c3cf19555e8fd1a29d012
amir <amirheydari9@gmail.com>
2021-06-14 05:49:30 -0700

    remove col 12 in tabs

--- src/views/pages/ViewRequest/Tabs/Submitted.vue
+++ src/views/pages/ViewRequest/Tabs/Submitted.vue
@@ -3,77 +3,75 @@
     class="mt-5"
     value="submitted"
   >
-    <v-col cols="12">
-      <v-data-table
-        :page="page"
-        :pageCount="numberOfPages"
-        :options.sync="options"
-        :server-items-length="totalItems"
-        :loading="loading"
-        :headers="headers"
-        :items="submitted"
-        no-results-text="اطلاعاتی یافت نشد"
-        class="w-100"
-        :items-per-page="5"
-        :footer-props="{
+    <v-data-table
+      :page="page"
+      :pageCount="numberOfPages"
+      :options.sync="options"
+      :server-items-length="totalItems"
+      :loading="loading"
+      :headers="headers"
+      :items="submitted"
+      no-results-text="اطلاعاتی یافت نشد"
+      class="w-100"
+      :items-per-page="5"
+      :footer-props="{
       showFirstLastPage: true,
       firstIcon: 'mdi-arrow-collapse-left',
       lastIcon: 'mdi-arrow-collapse-right',
       prevIcon: 'mdi-plus',
       nextIcon: 'mdi-minus'
     }"
-      >
-        <template v-slot:item.actions="{item}">
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on }">
-              <v-icon
-                v-on="on"
-                v-if="canSetStatusAndAssignToBrokerage"
-                small
-                class="mr-2"
-                @click="changeStatus(item)"
-              >
-                mdi-refresh
-              </v-icon>
-            </template>
-            <span>تغییر وضعیت</span>
-          </v-tooltip>
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on }">
-              <v-icon
-                v-on="on"
-                v-if="canSetStatusAndAssignToBrokerage"
-                small
-                class="mr-2"
-                @click="changeBrokerage(item)"
-              >
-                mdi-account-convert
-              </v-icon>
-            </template>
-            <span>ارجاع به کارگزاری</span>
-          </v-tooltip>
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on }">
-              <v-icon
-                v-on="on"
-                small
-                class="mr-2"
-                @click="seeDetails(item)"
-              >
-                mdi-eye
-              </v-icon>
-            </template>
-            <span>مشاهد جزئیات</span>
-          </v-tooltip>
-        </template>
-        <template v-slot:item.status="{ item }">
-          {{ transformRequestStatus(item.status) }}
-        </template>
-        <template v-slot:item.submitDate="{ item }">
-          {{ transformDateToJalali(item.submitDate) }}
-        </template>
-      </v-data-table>
-    </v-col>
+    >
+      <template v-slot:item.actions="{item}">
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+            <v-icon
+              v-on="on"
+              v-if="canSetStatusAndAssignToBrokerage"
+              small
+              class="mr-2"
+              @click="changeStatus(item)"
+            >
+              mdi-refresh
+            </v-icon>
+          </template>
+          <span>تغییر وضعیت</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+            <v-icon
+              v-on="on"
+              v-if="canSetStatusAndAssignToBrokerage"
+              small
+              class="mr-2"
+              @click="changeBrokerage(item)"
+            >
+              mdi-account-convert
+            </v-icon>
+          </template>
+          <span>ارجاع به کارگزاری</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+            <v-icon
+              v-on="on"
+              small
+              class="mr-2"
+              @click="seeDetails(item)"
+            >
+              mdi-eye
+            </v-icon>
+          </template>
+          <span>مشاهد جزئیات</span>
+        </v-tooltip>
+      </template>
+      <template v-slot:item.status="{ item }">
+        {{ transformRequestStatus(item.status) }}
+      </template>
+      <template v-slot:item.submitDate="{ item }">
+        {{ transformDateToJalali(item.submitDate) }}
+      </template>
+    </v-data-table>
   </v-tab-item>
 </template>
 
--- src/views/pages/ViewRequest/Tabs/Working.vue
+++ src/views/pages/ViewRequest/Tabs/Working.vue
@@ -3,63 +3,61 @@
     class="mt-5"
     value="working"
   >
-    <v-col cols="12">
-      <v-data-table
-        :page="page"
-        :pageCount="numberOfPages"
-        :options.sync="options"
-        :server-items-length="totalItems"
-        :loading="loading"
-        :headers="headers"
-        :items="working"
-        no-results-text="اطلاعاتی یافت نشد"
-        class="w-100"
-        :items-per-page="5"
-        :footer-props="{
+    <v-data-table
+      :page="page"
+      :pageCount="numberOfPages"
+      :options.sync="options"
+      :server-items-length="totalItems"
+      :loading="loading"
+      :headers="headers"
+      :items="working"
+      no-results-text="اطلاعاتی یافت نشد"
+      class="w-100"
+      :items-per-page="5"
+      :footer-props="{
       showFirstLastPage: true,
       firstIcon: 'mdi-arrow-collapse-left',
       lastIcon: 'mdi-arrow-collapse-right',
       prevIcon: 'mdi-plus',
       nextIcon: 'mdi-minus'
     }"
-      >
-        <template v-slot:item.status="{ item }">
-          {{ transformRequestStatus(item.status) }}
-        </template>
-        <template v-slot:item.submitDate="{ item }">
-          {{ transformDateToJalali(item.submitDate) }}
-        </template>
-        <template v-slot:item.actions="{item}">
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on }">
-              <v-icon
-                v-on="on"
-                v-if="canSetStatusAndAssignToBrokerage"
-                small
-                class="mr-2"
-                @click="changeStatus(item)"
-              >
-                mdi-refresh
-              </v-icon>
-            </template>
-            <span>تغییر وضعیت</span>
-          </v-tooltip>
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on }">
-              <v-icon
-                v-on="on"
-                small
-                class="mr-2"
-                @click="seeDetails(item)"
-              >
-                mdi-eye
-              </v-icon>
-            </template>
-            <span>مشاهد جزئیات</span>
-          </v-tooltip>
-        </template>
-      </v-data-table>
-    </v-col>
+    >
+      <template v-slot:item.status="{ item }">
+        {{ transformRequestStatus(item.status) }}
+      </template>
+      <template v-slot:item.submitDate="{ item }">
+        {{ transformDateToJalali(item.submitDate) }}
+      </template>
+      <template v-slot:item.actions="{item}">
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+            <v-icon
+              v-on="on"
+              v-if="canSetStatusAndAssignToBrokerage"
+              small
+              class="mr-2"
+              @click="changeStatus(item)"
+            >
+              mdi-refresh
+            </v-icon>
+          </template>
+          <span>تغییر وضعیت</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+            <v-icon
+              v-on="on"
+              small
+              class="mr-2"
+              @click="seeDetails(item)"
+            >
+              mdi-eye
+            </v-icon>
+          </template>
+          <span>مشاهد جزئیات</span>
+        </v-tooltip>
+      </template>
+    </v-data-table>
   </v-tab-item>
 </template>
 
@@ -99,7 +97,7 @@
         transformRequestStatus,
       }
     },
-    computed:{
+    computed: {
       canSetStatusAndAssignToBrokerage () {
         return permission().isSecretariant() && permission().isOrders()
       },
@@ -112,7 +110,7 @@
         },
       },
     },
-    methods:{
+    methods: {
       async readDataFromAPI () {
         this.loading = true
         const {
